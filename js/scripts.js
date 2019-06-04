19e972282a7217b8e4a95cb119ac228c90524acd
Anthony Golovin and Kelar Crisp <anthony@>
2019-06-04 15:00:40 -0700

    finished flashcards

---
+++ js/scripts.js
@@ -0,0 +1,86 @@
+$(function() {
+  $(".clickable1").click(function(){
+    $(".contentcards1").toggle();
+    $(".flashcard1").toggle();
+
+
+  });
+
+  $(".clickable2").click(function(){
+
+    $(".contentcards2").toggle();
+
+  });
+  $(".clickable3").click(function(){
+
+    $(".contentcards3").toggle();
+
+  });
+  $(".clickable4").click(function(){
+
+    $(".contentcards4").toggle();
+
+  });
+  $(".clickable5").click(function(){
+
+    $(".contentcards5").toggle();
+  });
+  $(".clickable6").click(function(){
+
+    $(".contentcards6").toggle();
+  });
+  $(".clickable7").click(function(){
+
+    $(".contentcards7").toggle();
+  });
+  $(".clickable8").click(function(){
+
+    $(".contentcards8").toggle();
+  });
+  $(".clickable9").click(function(){
+
+    $(".contentcard9").toggle();
+  });
+  $(".clickable10").click(function(){
+
+    $(".contentcards10").toggle();
+  });
+  $(".clickable11").click(function(){
+
+    $(".contentcards11").toggle();
+
+  });
+  $(".clickable11").click(function(){
+
+    $(".contentcards11").toggle();
+    
+  });
+  $(".clickable13").click(function(){
+
+    $(".contentcards13").toggle();
+  });
+  $(".clickable14").click(function(){
+
+    $(".contentcards14").toggle();
+  });
+  $(".clickable15").click(function(){
+
+    $(".contentcards15").toggle();
+  });
+  $(".clickable16").click(function(){
+
+    $(".contentcards16").toggle();
+  });
+  $(".clickable17").click(function(){
+
+    $(".contentcards17").toggle();
+  });
+  $(".clickable18").click(function(){
+
+    $(".contentcards18").toggle();
+  });
+  $(".clickable19").click(function(){
+
+    $(".contentcards19").toggle();
+  });
+  });
